ee09af3097fd9fb26cb182420f0524b9cfdd467a
github username <github email address>
2018-07-29 00:08:39 +0800

    Add slideUp() and slideDown(), show() and hide() for filter project feature

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,22 @@
 $('.menu-toggle').on('click', function() {
     $('.menu').toggleClass('open');
+    $('.main').toggleClass('translucent');
+  return false;
+})
+
+$('.sub-menu > a').on('click', function() {
+  let isVisible = $('ul ul').is(':visible');
+  if (isVisible === true) {
+    $('ul ul').slideUp();
+  } else {
+    $('ul ul').slideDown();
+  }
+  return false;
+})
+
+$('ul ul a').on('click', function() {
+  let filterValue = $(this).attr('data-filter');
+	$('.project').hide();
+  $(filterValue).show();
   return false;
 })
