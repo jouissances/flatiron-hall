de0e4f282c062a070687cf433268a5bbb6171270
github username <github email address>
2018-07-30 15:35:31 +0800

    Sort index page projects from latest date of submission

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,24 @@
 require './config/environment'
+require 'sinatra'
+require 'sinatra/flash'
 
 class ApplicationController < Sinatra::Base
 
     configure do
         enable :sessions
+        register Sinatra::Flash
         set :session_secret, 'hall_secret'
         set :public_folder, 'public'
         set :views, 'app/views'
     end
 
     get '/' do
-        @projects = Project.all
+        @projects = Project.all.sort { |x, y| y.id <=> x.id }
         erb :index
     end
 
     get '/home' do
-        @projects = Project.all
+        @projects = Project.all.sort { |x, y| y.id <=> x.id }
         erb :index
     end
 
