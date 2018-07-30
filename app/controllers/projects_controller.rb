998bd1ea9e299942f2fbb72eb0522b660a07f5af
github username <github email address>
2018-07-30 15:36:43 +0800

    Add flash messages. Add placeholder image. Enable slug-based search

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController    
-    
+
     get '/projects' do
         if logged_in?
           @user = current_user
@@ -15,6 +15,7 @@ class ProjectsController < ApplicationController
         if logged_in?
           erb :'projects/create'
         else
+          flash[:error] = "Please register or sign in to submit a project."
           redirect '/login'
         end
     end
@@ -22,34 +23,25 @@ class ProjectsController < ApplicationController
     post '/projects' do
         if logged_in?
             @user = current_user
-            if params[:title] == ""
+            puts params
+            if params.except(:video_url).values.any? { |val| val == "" }
               redirect '/projects/new'
+            elsif params[:video_url] == ''
+              params[:video_url] = "https://previews.dropbox.com/p/orig/AALLu4qexe4eU7T5bGxzyH9_PR8rB915D7agQUw_uIzME3onBZDmO12fowl6_McLBUqDARxX3KSMvEATbxJee989K67r4KKGASU1-9165ihl1tIUQXq59G-3vHdEKiTI2J2WfY2Vu9hP3GYc_Iew4HQx7aaOYTLugxx-m9Xj6ENJmNCmYfcOIdKId_kGbeGysGcCYA_WpGkP-fCT_ClrTCkt/p.gif?size=1024x768&size_mode=3"
+              # Project.columns_hash['video_url'].default
+              @project = Project.create(:id => params[:id], :title => params[:title], :category => params[:category], :description => params[:description], :github => params[:github], :external_uri => params[:external_uri], :user_id => session[:user_id], :video_url => params[:video_url], :blog_url => params[:blog_url], :created_at => params[:created_at])
+              redirect to "/projects/#{@project.slug}"
             else
-              if params[:video_url] == ""
-                params[:video_url] = Project.columns_hash['video_url'].default
-              end
-              @project = Project.create(:id => params[:id], :title => params[:title], :category => params[:category], :description => params[:description], :github => params[:github], :external_uri => params[:external_uri], :user_id => session[:user_id], :video_url => params[:video_url])
-              redirect to "/projects/#{@project.id}"
+              @project = Project.create(:id => params[:id], :title => params[:title], :category => params[:category], :description => params[:description], :github => params[:github], :external_uri => params[:external_uri], :user_id => session[:user_id], :video_url => params[:video_url], :blog_url => params[:blog_url], :created_at => params[:created_at])
+              redirect to "/projects/#{@project.slug}"
             end
         else
             redirect '/login'
         end
     end
 
-    get '/projects/:id' do
-      if logged_in?
-        @user = current_user
-        @project = Project.find(params[:id])
-        erb :'projects/show'
-      else
-        redirect to '/login'
-      end
-    end
-
     get '/projects/:slug' do
       if logged_in?
-        @user = current_user
-        @projects = Project.all
         @project = Project.find_by_slug(params[:slug])
         erb :'projects/show'
       else
@@ -78,7 +70,7 @@ class ProjectsController < ApplicationController
             redirect to "/projects/#{@project.id}/edit"
           else
             if @project && @project.user == current_user
-              @project.update(:title => params[:title], :category => params[:category], :description => params[:description], :github => params[:github], :external_uri => params[:external_uri], :video_url => params[:video_url])
+              @project.update(:title => params[:title], :category => params[:category], :description => params[:description], :github => params[:github], :external_uri => params[:external_uri], :video_url => params[:video_url], :blog_url => params[:blog_url])
               redirect to "/projects/#{@project.id}"
             else
               redirect to '/projects'
