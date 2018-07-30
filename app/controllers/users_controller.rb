861a0fac436a3538da0d387a996bdcfee7439dc7
github username <github email address>
2018-07-30 15:37:18 +0800

    Add flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,19 +10,21 @@ class UsersController < ApplicationController
     
     post '/register' do
         if params.values.any? { |val| val == "" }
+            flash[:error] = "Please fill out all fields."
             redirect '/register'
         else
             @user = User.create(params)
             session[:user_id] = @user.id
+            flash[:success] = "You have successfully registered."
             redirect '/projects'
         end
     end
     
     get '/login' do
         if !logged_in?
-          erb :'users/login'
+            erb :'users/login'
         else
-          redirect '/projects'
+            redirect '/projects'
         end
     end
     
@@ -30,8 +32,14 @@ class UsersController < ApplicationController
         @user = User.find_by(:username => params[:username])
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
+            flash[:success] = "You have successfully logged in."
             redirect '/projects'
+        elsif
+            params.values.any? { |val| val == "" }
+            flash[:error] = "Please fill out all fields."
+            redirect '/login'
         else
+            flash[:error] = "Username and password does not match. Please try again."
             redirect '/login'
         end  
     end
@@ -39,9 +47,10 @@ class UsersController < ApplicationController
     get '/users/:slug' do
         if logged_in?
             @user = User.find_by_slug(params[:slug])
-            @projects = Project.all
+            @projects = Project.find_by_user_id(@user.id)            
             erb :'users/show'
         else
+            flash[:error] = "Please register or log in to view this user's profile."
             redirect to '/login'
         end
     end
@@ -49,9 +58,10 @@ class UsersController < ApplicationController
     get '/logout' do 
         if session[:user_id] != nil
           session.destroy
+          flash[:notice] = "You have successfully logged out."
           redirect '/login'
         else
-          redirect '/projects'
+          redirect '/login'
         end
     end
 
