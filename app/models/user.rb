50fbbb599fd71a810eb587549584868ae788ec2e
github username <github email address>
2018-07-30 15:38:00 +0800

    Validate username and email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
     has_many :projects
 
     validates_presence_of :username, :email, :password_digest
+    validates_uniqueness_of :username, { message: 'This username has already been taken' }
+    validates_uniqueness_of :email, { message: 'This e-mail has already been registered' }
     has_secure_password
 
     def slug
